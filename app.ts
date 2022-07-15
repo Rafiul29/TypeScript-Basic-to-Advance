42a668db23037827d6eff69299868f7f41ff3f02
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-15 10:18:12 +0600

    Typescript

--- app.ts
+++ app.ts
@@ -4,17 +4,23 @@ function add (n1: number,n2: number,showResult: boolean, resultParse: string): n
     // if( typeof n1 !== 'number' || typeof n2  !== 'number'){
     //     throw new Error("Incorrect Input");
     // }
+
+
     const result = n1+n2;
     if(showResult){
         console.log( resultParse +result);
     }else{
         return  result;
     }
-}
 
+
+
+}
 let number1= 4;
 let number2 = 5;
 let printResult = true;
 let resultParse = 'Result is ';
+
+
 
  add(number1,number2, printResult,resultParse)
